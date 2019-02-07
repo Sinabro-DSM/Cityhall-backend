35d3e37d74c4340817e124635bcd1bfa52b3711e
TDM-RE <nye7181@gmail.com>
2019-02-07 14:21:39 +0900

    프로젝트 구조 작성

---
+++ bin/www.js
@@ -0,0 +1,32 @@
+const app = require('../app');
+const http = require('http');
+
+const port = process.env.PORT || '3000';
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
